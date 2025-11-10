4906927e0948ca721b4cbbeee09b4475734553e3
din <surabordin@windowslive.com>
2025-11-10 08:57:24 +0700

    feat: initial get request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::{self};
+use anyhow::Result;
+use dotenv::dotenv;
+
+async fn get_request() -> Result<()>{
+    let response = reqwest::get("https://test.deribit.com/api/v2").await?;
+    println!("{}", response.status());
+
+    let response_body = response.text().await?;
+    println!("\n{}", response_body);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+    get_request().await?;
+    Ok(())
 }
