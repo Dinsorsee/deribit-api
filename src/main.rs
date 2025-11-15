b5c3b8263b9a736749b7fa862b9faf0bf9be7cd9
Dinsorsee <surabordin@windowslive.com>
2025-11-15 20:54:47 +0700

    feat: install reqwest and serde for connect deribit testnet url (#1)
    * feat: initial get request
    
    * feat: use dotenv and std env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::{self};
+use anyhow::Result;
+use dotenv::dotenv;
+use std::env;
+
+async fn get_request() -> Result<()>{
+    dotenv().ok();
+
+    let response = reqwest::get(env::var("URL").expect("URL Not found")).await?;
+    println!("{}", response.status());
+
+    let response_body = response.text().await?;
+    println!("\n{}", response_body);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+    get_request().await?;
+    Ok(())
 }
