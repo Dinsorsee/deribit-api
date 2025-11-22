bb176dea47f180b5e18214c62a445d29804fbb68
Dinsorsee <surabordin@windowslive.com>
2025-11-22 16:19:56 +0700

    feat: import json rpc response struct from deribit model

---
+++ src/models/jsonrpc.rs
@@ -0,0 +1,47 @@
+use crate::models::{Either, Request};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub struct JSONRPCRequest<Q: Request> {
+    pub id: i64,
+    pub method: String,
+    #[serde(skip_serializing_if = "crate::models::Request::no_payload")]
+    pub params: Q,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct JSONRPCResponse<R> {
+    pub jsonrpc: JSONRPCVersion,
+    pub id: i64,
+    pub testnet: bool,
+    #[serde(alias = "error")]
+    pub result: Either<R, ErrorDetail>,
+    pub us_in: u64,
+    pub us_out: u64,
+    pub us_diff: u64,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct JSONRPCSuccessResponse<R> {
+    pub jsonrpc: JSONRPCVersion,
+    pub id: i64,
+    pub testnet: bool,
+    pub result: R,
+    pub us_in: u64,
+    pub us_out: u64,
+    pub us_diff: u64,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, Copy)]
+pub enum JSONRPCVersion {
+    #[serde(rename = "2.0")]
+    V2,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub struct ErrorDetail {
+    pub code: i64,
+    pub message: String,
+}
---
+++ src/models/mod.rs
@@ -0,0 +1,96 @@
+pub mod jsonrpc;
+
+use serde::{Deserialize, Serialize};
+use std::result::Result as StdResult;
+
+//Implementing 'Request' and 'Either' from
+//https://github.com/dovahcrow/deribit-rs/blob/master/src/models/mod.rs
+pub trait Request {
+    const METHOD: &'static str;
+    const HAS_PAYLOAD: bool = true;
+    type Response;
+
+    fn no_payload(&self) -> bool {
+        !Self::HAS_PAYLOAD
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum Either<L, R> {
+    Left(L),
+    Right(R),
+}
+
+impl<L, R> Either<L, R> {
+    pub fn map_left<F, U>(self, f: F) -> Either<U, R>
+    where
+        F: FnOnce(L) -> U,
+    {
+        match self {
+            Either::Left(l) => Either::Left(f(l)),
+            Either::Right(r) => Either::Right(r),
+        }
+    }
+
+    pub fn map_right<F, U>(self, f: F) -> Either<L, U>
+    where
+        F: FnOnce(R) -> U,
+    {
+        match self {
+            Either::Right(r) => Either::Right(f(r)),
+            Either::Left(l) => Either::Left(l),
+        }
+    }
+    pub fn left_result(self) -> StdResult<L, R> {
+        match self {
+            Either::Left(l) => Ok(l),
+            Either::Right(r) => Err(r),
+        }
+    }
+    pub fn right_result(self) -> StdResult<R, L> {
+        match self {
+            Either::Left(l) => Err(l),
+            Either::Right(r) => Ok(r),
+        }
+    }
+}
+
+impl<T> Either<T, T> {
+    pub fn unwrap(self) -> T {
+        match self {
+            Either::Left(l) => l,
+            Either::Right(r) => r,
+        }
+    }
+}
+
+impl<L, R> Either<L, R> {
+    pub fn unwrap_left(self) -> L {
+        match self {
+            Either::Left(l) => l,
+            Either::Right(_) => panic!("Either is right"),
+        }
+    }
+
+    pub fn left(self) -> Option<L> {
+        match self {
+            Either::Left(l) => Some(l),
+            Either::Right(_) => None,
+        }
+    }
+
+    pub fn unwrap_right(self) -> R {
+        match self {
+            Either::Left(_) => panic!("Either is left"),
+            Either::Right(r) => r,
+        }
+    }
+
+    pub fn right(self) -> Option<R> {
+        match self {
+            Either::Left(_) => None,
+            Either::Right(r) => Some(r),
+        }
+    }
+}
