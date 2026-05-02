3fb792b08794e3bb60d224e12f04e8473a2ec294
din <surabordin@windowslive.com>
2026-05-02 15:42:47 +0700

    feat: implement deribit client

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use deribit_api::config::loader;
-use deribit_api::infrastructure::deribit::client::{get_index_price, get_token};
+use deribit_api::domain::models::market::CurrencyPair;
+use deribit_api::infrastructure::deribit::client::DeribitClient;
 use tracing::{error, info};
 
 #[tokio::main]
@@ -9,17 +10,23 @@ async fn main() -> Result<()> {
         .with_max_level(tracing::Level::DEBUG)
         .init();
 
-    let dotenvy_env = match loader::load() {
+    let app_config = match loader::load() {
         Ok(env) => env,
         Err(e) => {
             error!("Failed to load ENV: {}", e);
             std::process::exit(1)
         }
     };
-
     info!("Loaded ENV config: success");
 
-    get_token(&dotenvy_env.deribit_api.url).await?;
-    get_index_price(&dotenvy_env.deribit_api.url).await?;
+    let url = app_config.deribit.url;
+    let client_id = app_config.deribit.client_id;
+    let client_secret = app_config.deribit.client_secret;
+
+    let client = DeribitClient::new(url, client_id, client_secret);
+    let pair = CurrencyPair::BtcUsdt;
+
+    client.authenticate().await?;
+    client.get_index_price(pair).await?;
     Ok(())
 }
